f4b97d3eec6b1289496a6f47da207007e0c1b2c0
matuyama1223 <matu1223@icloud.com>
2020-09-04 15:24:55 +0000

    [Add]

--- app/models/book.rb
+++ app/models/book.rb
@@ -12,8 +12,6 @@ class Book < ApplicationRecord
 	def favorited_by?(user)
 		favorites.where(user_id: user.id).exists?
 	end
-
-
 	def self.search(search)
     if search
       Book.where(['title LIKE ?', "%#{search}%"])
